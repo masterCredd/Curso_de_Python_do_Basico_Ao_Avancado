0f8e2ceccf6983e4ef5a7912c7ecfccc77daf6aa
Edgar Soares <ed.soares@hotmail.com>
2023-08-01 15:12:40 -0300

    346

---
+++ curso_de_python_do_basico_ao_avancado/Secao_7/README.md
@@ -0,0 +1,5 @@
+# Exemplo de uma calculadora feito em PySide6
+
+---
+---
+---
--- curso_de_python_do_basico_ao_avancado/Secao_7/calculadora.py
+++ curso_de_python_do_basico_ao_avancado/Secao_7/calculadora.py
@@ -1,12 +1,1 @@
-import sys
 
-from PySide6.QtWidgets import QApplication, QPushButton
-
-app = QApplication(sys.argv)
-
-botao = QPushButton('Texto do botão')
-botao.setStyleSheet('font-size: 40px;')
-botao.show()  # adiciona o Widget na hierarquia e exibe a janela
-
-
-app.exec()  # o loop da aplicação
